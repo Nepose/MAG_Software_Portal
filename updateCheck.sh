c57ad59b08e2404bc4aa3176b9510e8a60826d92
Nepose <nepose.magportal@gmail.com>
2019-02-22 23:24:01 +0100

    Upgrade 2.00.1

--- updateCheck.sh
+++ updateCheck.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-export STBSDK_Available_Ver="2.00-beta"
-export STBSDK_Available_Changelog="No changes in update."
+export STBSDK_Available_Ver="2.00.1"
+export STBSDK_Available_Changelog="This upgrade fixes two bugs related to upgrading and building firmware."
